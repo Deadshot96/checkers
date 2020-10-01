590f00f497cd61bcc71de4797b5a22d38bd1b139
Bhushan <bhushan1810patil@gmail.com>
2020-10-02 00:43:10 +0530

    create checker.py

---
+++ Block.py
@@ -0,0 +1,128 @@
+import pygame
+import colors
+from settings import *
+
+pygame.init()
+pygame.font.init()
+
+CROWN = pygame.transform.scale(pygame.image.load(CROWN_PATH), (RADIUS, RADIUS))
+
+
+class Block:
+
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+        self.size = SIZE
+        self.padding = PADDING
+        self.x = self.col * self.size
+        self.y = self.row * self.size
+        self.player = None
+        self.isKing = False
+        self.radius = RADIUS
+        self.direction = list()
+        self.currentBlock = None
+        self.isValid = False
+        self.isEmpty = True
+        self.selected = False
+
+    def is_king(self):
+        return self.isKing
+
+    def make_king(self):
+        self.isKing = True
+        self.direction = [1, -1]
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def get_direction(self):
+        return self.direction
+
+    def is_black(self):
+        return self.color == colors.BLACK
+
+    def is_empty(self):
+        return self.player is None
+
+    def make_valid(self):
+        self.isValid = True
+
+    def make_invalid(self):
+        self.isValid = False
+
+    def make_player(self, player):
+        self.player = player
+
+        if player == colors.BLUE:
+            self.direction = [1]
+        elif player == colors.RED:
+            self.direction = [-1]
+
+        self.isEmpty = False
+
+    def remove_player(self):
+        self.player = None
+        self.direction = []
+        self.isEmpty = True
+
+    def select(self):
+        self.selected = True
+
+    def is_selected(self):
+        return self.selected
+
+    def deselect(self):
+        self.selected = False
+
+    def occupy(self):
+        self.isEmpty = False
+
+    def vacant(self):
+        self.isEmpty = True
+
+    def is_occupied(self):
+        return self.isEmpty
+
+    def __repr__(self):
+        if self.player == colors.RED:
+            return "RED"
+        elif self.player == colors.BLUE:
+            return "BLUE"
+        else:
+            return "EMPTY"
+
+    def draw(self, win):
+        rect = (self.x, self.y, self.size, self.size)
+        pygame.draw.rect(win, self.color, rect)
+
+        centerX, centerY = self.x + self.size // 2, self.y + self.size // 2
+
+        if self.isValid:
+            pygame.draw.circle(win, colors.CORN_FLOWER_BLUE,
+                               (centerX, centerY), self.radius // 2)
+
+        if self.player is not None:
+
+            if self.is_selected():
+                color = colors.LAWN_GREEN
+                surface = pygame.Surface((self.size, self.size))
+                surface.set_colorkey(colors.KEY)
+                surface.set_alpha(128)
+                surface.fill(color)
+                rect = surface.get_rect()
+                rect.center = centerX, centerY
+
+                win.blit(surface, rect)
+
+            pygame.draw.circle(
+                win, self.player, (centerX, centerY), self.radius)
+            pygame.draw.circle(win, colors.WHITE,
+                               (centerX, centerY), self.radius + 1, 1)
+
+            if self.is_king():
+                rect = CROWN.get_rect()
+                rect.center = centerX, centerY
+
+                win.blit(CROWN, rect)
---
+++ checker.py
@@ -0,0 +1,225 @@
+import pygame
+import random
+import math
+import colors
+import os
+import itertools
+from Block import *
+from settings import *
+
+pygame.init()
+pygame.font.init()
+
+
+class Game:
+
+    FPS = 60
+    COOLDOWN = 20
+    COLORS = [colors.RED, colors.BLACK]
+    PLAYERS = [colors.BLUE, colors.RED]
+    TURNS = ['RED', 'BLUE']
+
+    def __init__(self):
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.rows = NROW
+        self.cols = NCOL
+        self.gameWidth = GAMEWIDTH
+        self.gameHeight = GAMEHEIGHT
+        self.size = SIZE
+        self.xoff = (self.width - self.gameWidth) // 2
+        self.yoff = int((self.height - self.gameHeight) * 0.8)
+        self.cooldown_index = 0
+        self.grid = None
+        self.clock = None
+        self.titleFont = None
+
+    def game_init(self):
+
+        self.win = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Checkers Developer")
+
+        self.gameWin = self.win.subsurface(
+            (self.xoff, self.yoff, self.gameWidth, self.gameHeight))
+        self.win.fill(colors.MID_BLACK)
+        self.gameWin.fill(colors.BLACK)
+
+        pygame.display.update()
+
+        self.clock = pygame.time.Clock()
+        self.titleFont = pygame.font.SysFont("comicsansms", 40)
+
+        titleText = self.titleFont.render("Checkers Developer", 1, colors.GOLD)
+        wid, hgt = titleText.get_size()
+        self.win.blit(titleText, ((self.width - wid) //
+                                  2, (self.yoff - hgt) // 2))
+
+        self.grid_init()
+
+    def grid_init(self):
+        self.grid = list()
+        count = -1
+        for row in range(self.rows):
+            count += 1
+            self.grid.append(list())
+            for col in range(self.cols):
+                color = self.COLORS[count % 2]
+                self.grid[row].append(Block(row, col, color))
+                count += 1
+
+    def new_game(self):
+
+        self.blueKing = 0
+        self.redKing = 0
+        self.numBlue = 12
+        self.numRed = 12
+        self.CYCLE = itertools.cycle(self.TURNS)
+        self.turn = next(self.CYCLE)
+        self.selected = None
+        self.valid_positions = dict()
+        self.num_valid_moves = 0
+
+        # Reset Index
+        for rowIndex, row in enumerate(self.grid):
+            for colIndex, spot in enumerate(row):
+                if spot.is_black():
+                    if rowIndex < 3:
+                        spot.make_player(self.PLAYERS[0])
+                    elif rowIndex > 4:
+                        spot.make_player(self.PLAYERS[1])
+                    else:
+                        spot.remove_player()
+
+    def get_pos(self, pos):
+        x, y = pos
+        x -= self.xoff
+        y -= self.yoff
+
+        return y // self.size, x // self.size
+
+    def cooldown(self):
+        if self.cooldown_index > self.COOLDOWN:
+            self.cooldown_index = 0
+        elif self.cooldown_index > 0:
+            self.cooldown_index += 1
+
+    def draw_grid(self, win):
+        for row in self.grid:
+            for spot in row:
+                spot.draw(win)
+
+    def is_cool(self):
+        self.cooldown_index == 0
+
+    def show_positions(self):
+        row, col = self.selected.get_pos()
+        self._traverse(self.selected)
+        print(self.valid_positions)
+
+    def _traverse(self, piece, isSkipped=False):
+        row, col = piece.get_pos()
+        name = str(piece)
+
+        print("In Traverse")
+        if not isSkipped:
+            for rowDelta in piece.direction:
+                for colDelta in [-1, 1]:
+                    nRow = row + rowDelta
+                    nCol = col + colDelta
+
+                    if self.is_valid_dims(nRow, nCol):
+                        dest = self.grid[nRow][nCol]
+                        destName = str(dest)
+
+                        if destName == "EMPTY":
+                            self.valid_positions[dest] = None
+                            dest.make_valid()
+
+                        if destName != self.turn:
+                            jumpRow = nRow + rowDelta
+                            jumpCol = nCol + colDelta
+
+                            if self.is_valid_dims(jumpRow, jumpCol):
+                                jump = self.grid[jumpRow][jumpCol]
+
+                                if str(jump) == "EMPTY":
+                                    jump.make_valid()
+                                    self.valid_positions[jump] = [dest]
+
+        else:
+            rowDelta = []
+
+    def deselect(self):
+        self.selected.deselect()
+        self.selected = None
+        for block in self.valid_positions.keys():
+            block.make_invalid()
+
+        self.valid_positions = dict()
+
+    def game_move(self, row, col):
+        piece = self.grid[row][col]
+        destName = str(piece)
+
+        if destName == self.turn and self.selected is None:
+            self.selected = piece
+            self.selected.select()
+            self.show_positions()
+
+        elif destName == self.turn:
+            self.deselect()
+            self.selected = piece
+            self.selected.select()
+            self.show_positions()
+
+        elif destName.lower() == 'empty':
+            if piece in self.valid_positions():
+                self.move_piece(piece)
+            else:
+                self.deselect()
+
+        else:
+            self.deselect()
+
+    def move_piece(self, dest):
+        pass
+
+    def draw(self, win):
+        self.draw_grid(win)
+        pygame.display.update()
+
+    def is_valid_dims(self, row, col):
+        return row in range(NROW) and col in range(NCOL)
+
+    def run(self):
+        self.game_init()
+        self.new_game()
+
+        run = True
+        while run:
+            self.clock.tick(self.FPS)
+            self.draw(self.gameWin)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pressed = pygame.mouse.get_pressed()
+
+                    if pressed[0]:
+                        pos = pygame.mouse.get_pos()
+                        row, col = self.get_pos(pos)
+
+                        if self.is_valid_dims(row, col):
+                            self.game_move(row, col)
+
+                        print(self.grid[row][col])
+
+        pygame.font.quit()
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    X = Game()
+    X.run()
